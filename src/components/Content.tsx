5282123cb9cd4ff295e7fc76054d7cf8785599c3
bsfonseca <bs.fonseca12@gmail.com>
2024-01-24 11:34:40 -0300

    feat: criação card menu

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,9 +1,132 @@
+import styled from "styled-components";
+
+const ContentStyled = styled.div`
+    flex: 1;
+
+    overflow: auto;
+    min-width: 220px;
+    background-color: white;
+    border: 1px solid #bdbbbb;
+
+    #pagina-inicial {
+        color: #4a4141;
+        font-size: 24px;
+    }
+`;
+
 export function Content() {
     return (
         <>
-            <main id="content">
-                <p>Content</p>
-            </main>
+            <ContentStyled>
+                <p id="pagina-inicial">Página Inicial</p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+                <hr />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod corporis impedit natus recusandae
+                    quisquam quia odit quam voluptatum quae aut.
+                </p>
+            </ContentStyled>
         </>
     );
 }
