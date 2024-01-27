4e65e675ecb43af8a8d485699c6dc128424fec55
bsfonseca <bs.fonseca12@gmail.com>
2024-01-26 22:08:37 -0300

    feat: chamada de API para listar tweets e controle de autenticação de usuários

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,54 +4,38 @@ import { Content } from "../components/Content";
 import { LeftMenu } from "../components/LeftMenu";
 import { RightMenu } from "../components/RightMenu";
 import { Tweet } from "../models/tweet.model";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { User } from "../models/user.model";
 
 export const Home = () => {
+    const [user, setUser] = useState<User>();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const userStorage = localStorage.getItem("user");
+
+        if (!userStorage) {
+            alert("Sessão experida, faça login novamente");
+            navigate("/login");
+            return;
+        }
+
+        setUser(JSON.parse(userStorage));
+    }, []);
+
     //chamar api
     const [listaTweets, setTweets] = useState<Tweet[]>([]);
 
-    const user: User = {
-        id: "4348433",
-        name: "Bruna",
-        urlPhoto:
-            "https://lh3.googleusercontent.com/pw/ABLVV87pC-tZ2XFj3JYHc9OCgCFPgSbTMCL4DGsHT92jPpLEqKjXlJSAE1dxgzbF-V9hZL31dSucjdZ6_wCqD28e_4FHOGLgw6LCbp7l8nLh8qgfgrmJ691yj40DqEIe8beeoqTc86oj16QYhzbtezA4wh-X=w709-h945-s-no-gm",
-        username: "@bruna",
-    };
-
     useEffect(() => {
-        setTweets([
-            {
-                content: "lalaalla",
-                tipo: "N",
-                id: "355353",
-                user: user,
-            },
-            {
-                content: "passear",
-                tipo: "N",
-                id: "9706353",
-                user: user,
-            },
-            {
-                content: "dnsafausfhaufbafa",
-                tipo: "N",
-                id: "212415353",
-                user: user,
-            },
-            {
-                content: "passear",
-                tipo: "N",
-                id: "9706353",
-                user: user,
-            },
-            {
-                content: "dnsafausfhaufbafa",
-                tipo: "N",
-                id: "212415353",
-                user: user,
-            },
-        ]);
-    }, []);
+        if (!user) {
+            return;
+        }
+
+        axios.get("http://localhost:3333/tweets").then((result) => {
+            setTweets(result.data.data);
+        });
+    }, [user]);
 
     return (
         <>
@@ -66,3 +50,5 @@ export const Home = () => {
         </>
     );
 };
+
+//then: recebe um ok do AXIOS
