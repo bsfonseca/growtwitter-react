92486ea10f5fed7e67350a6fe536f1f573c5a5d0
bsfonseca <bs.fonseca12@gmail.com>
2024-01-30 16:27:06 -0300

    feat: adição de função para criar tweet

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { User } from "../models/user.model";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASEURL,
@@ -23,5 +24,19 @@ export async function listarTudo() {
             alert(error.toString());
             return null;
         }
+    }
+}
+
+export async function criarTweet(body: any, user: User) {
+    try {
+        const result = await api.post(`/usuario/${user.id}/tweet`, body, {
+            headers: {
+                Authorization: user.token,
+            },
+        });
+        return result.data.data;
+    } catch (error: any) {
+        alert(error.toString());
+        return null;
     }
 }
